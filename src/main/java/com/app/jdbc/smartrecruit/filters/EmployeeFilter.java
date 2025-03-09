dd3a223ca1cc0efec9ab5ec1b61b286dadc4c538
Th3you7 <elbasiriy@gmail.com>
2025-03-09 22:16:40 +0000

    filters & authrization

--- src/main/java/com/app/jdbc/smartrecruit/filters/EmployeeFilter.java
+++ src/main/java/com/app/jdbc/smartrecruit/filters/EmployeeFilter.java
@@ -1,4 +1,34 @@
 package com.app.jdbc.smartrecruit.filters;
 
-public class EmployeeFilter {
+import com.app.jdbc.smartrecruit.models.Admin;
+import com.app.jdbc.smartrecruit.models.Employee;
+import com.app.jdbc.smartrecruit.models.Recruiter;
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+@WebFilter({"/employee/*"})
+public class EmployeeFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        HttpSession session = request.getSession(false);
+
+        // check if user and user's typeof
+        if (session == null || session.getAttribute("user") == null) {
+            response.sendRedirect("/auth/login-form");
+        } else if (session.getAttribute("user") instanceof Admin) {
+            response.sendRedirect("/admin/dashboard");
+        } else if (session.getAttribute("user") instanceof Recruiter) {
+            response.sendRedirect("/recruiter/dashboard");
+        } else {
+            filterChain.doFilter(request, response);
+        }
+    }
 }
